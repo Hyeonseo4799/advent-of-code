efa619386d304a46c8f986c995212d58721b7490
hyeonseo <s21035@gsm.hs.kr>
2023-12-01 22:31:35 +0900

    :memo: :: Day01 - Part two

--- src/Day01.kt
+++ src/Day01.kt
@@ -9,9 +9,28 @@ fun main() {
         }
     }
 
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 142)
+    fun part2(input: List<String>): Int {
+        val numbers = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+            .mapIndexed { index, number -> (index + 1) to number }
+
+        val converted = input.map { line ->
+            line.fold("") { current, next ->
+                val digit = "$current$next"
+                numbers.find { (_, number) -> digit.contains(number) }
+                    ?.let { (value, number) -> digit.replace(number, "$value$next")}
+                    ?: "$current$next"
+            }
+        }
+        return part1(converted)
+    }
+
+
+    val testInput1 = readInput("Day01_test1")
+    val testInput2 = readInput("Day01_test2")
+    check(part1(testInput1) == 142)
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
+    part2(input).println()
 }
