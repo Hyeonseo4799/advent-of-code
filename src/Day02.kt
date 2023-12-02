121fed3a25f42493b922275683585e054fc7a169
hyeonseo <s21035@gsm.hs.kr>
2023-12-02 21:28:53 +0900

    :memo: :: Day02 - Part two

--- src/Day02.kt
+++ src/Day02.kt
@@ -30,9 +30,39 @@ fun main() {
         }
     }
 
+    fun part2(input: List<String>): Int {
+        return input.sumOf { game ->
+            val min = game.substringAfter(":")
+                .replace(" ", "")
+                .split(";")
+                .map { line ->
+                    line.split(",").let { colors ->
+                        Triple(
+                            colors.find { it.contains("red") }
+                                ?.substringBefore("red")?.toInt() ?: 0,
+                            colors.find { it.contains("green") }
+                                ?.substringBefore("green")?.toInt() ?: 0,
+                            colors.find { it.contains("blue") }
+                                ?.substringBefore("blue")?.toInt() ?: 0
+                        )
+                    }
+                }.let { rgb ->
+                    Triple(
+                        rgb.maxBy { it.first }.first,
+                        rgb.maxBy { it.second }.second,
+                        rgb.maxBy { it.third }.third
+                    )
+                }
+            min.first * min.second * min.third
+        }
+    }
+
     val testInput1 = readInput("Day02_test1")
     check(part1(testInput1, Triple(12, 13, 14)) == 8)
+    val testInput2 = readInput("Day02_test2")
+    check(part2(testInput2) == 2286)
 
     val input = readInput("Day02")
     part1(input, Triple(12, 13, 14)).println()
+    part2(input).println()
 }
