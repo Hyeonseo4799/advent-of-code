04152e3cab4d78aad872e8e6ff3a8419c6899f3a
Hyeonseo4799 <s21035@gsm.hs.kr>
2023-12-05 15:44:42 +0900

    :memo: :: Day03 - Part two

--- src/Day03.kt
+++ src/Day03.kt
@@ -23,10 +23,41 @@ fun main() {
         }
     }
 
+    fun part2(input: List<String>): Int {
+        return input.flatMapIndexed { y, line ->
+            """\d+""".toRegex()
+                .findAll(line)
+                .map { Number(it.value, it.range.first, y) }
+        }.let { numbers ->
+            input.mapIndexed { y, line ->
+                line.withIndex()
+                    .filter { it.value == '*' }
+                    .map {
+                        val x = it.index
+                        numbers.filter { number ->
+                            val numberX = listOf(number.x, number.x + number.value.lastIndex)
+
+                            number.y in (y - 1)..(y + 1) && numberX.any { c -> c in (x - 1)..(x + 1) }
+                        }
+                    }
+                    .filter { it.size == 2 }
+                    .takeIf { it.isNotEmpty() }
+                    ?.let { near ->
+                        near.sumOf { it[0].value.toInt() * it[1].value.toInt() }
+                    }
+            }
+        }
+            .filterNotNull()
+            .sum()
+    }
+
     val testInput = readInput("Day03_test1")
     check(part1(testInput) == 4361)
+    check(part2(testInput) == 467835)
+
 
     val input = readInput("Day03")
     part1(input).println()
+    part2(input).println()
 }
 
