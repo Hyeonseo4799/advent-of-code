1d07c332018c814c827572e3ea15cd8a6f882118
Hyeonseo4799 <s21035@gsm.hs.kr>
2023-12-13 10:11:12 +0900

    :memo: :: Day04 - Part two

--- src/Day04.kt
+++ src/Day04.kt
@@ -14,10 +14,32 @@ fun main() {
         }
     }
 
+    fun part2(input: List<String>): Int {
+        val instances = MutableList(input.size) { 1 }
+
+        input.mapIndexed { currentIdx, line ->
+            val cards = line.substringAfter(":")
+                .replace("|", "")
+                .split(" ")
+                .filter { it.isNotEmpty() }
+                .groupBy { it }
+                .filter { it.value.size > 1 }
+                .values
+
+            cards.forEachIndexed { index, _ ->
+                val cardIdx = currentIdx + (index + 1)
+                instances[cardIdx] += instances[currentIdx]
+            }
+        }
+        return instances.sum()
+    }
+
     val testInput = readInput("Day04_test1")
     check(part1(testInput) == 13)
+    check(part2(testInput) == 30)
 
     val input = readInput("Day04")
     part1(input).println()
+    part2(input).println()
 }
 
