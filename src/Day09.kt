c6fe71dfc589649dcb102c68d670250e26fc4588
Hyeonseo4799 <s21035@gsm.hs.kr>
2023-12-18 15:59:57 +0900

    :memo: :: Day09 - Part two

--- src/Day09.kt
+++ src/Day09.kt
@@ -20,9 +20,32 @@ fun main() {
         }
     }
 
+    fun part2(input: List<String>): Int {
+        return input.sumOf { line ->
+            line.split(" ")
+                .map(String::toInt)
+                .reversed()
+                .let { numbers ->
+                    val differenceList = mutableListOf(numbers)
+
+                    while (differenceList.last().any { it != 0 }) {
+                        val current = differenceList.last()
+                        val newDifference = current.zipWithNext { a, b -> b - a }
+
+                        differenceList.add(newDifference)
+                    }
+                    differenceList
+                }
+                .reversed()
+                .fold(0) { sum, numbers -> sum + numbers.last() }
+                .toInt()
+        }
+    }
+
     val testInput = readInput("Day09_test1")
     check(part1(testInput) == 114)
 
     val input = readInput("Day09")
     part1(input).println()
+    part2(input).println()
 }
