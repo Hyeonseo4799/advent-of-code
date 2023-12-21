f7a301f62f25fdb2329d760ecbdde178cb49b367
Hyeonseo4799 <s21035@gsm.hs.kr>
2023-12-21 10:05:11 +0900

    :memo: :: Day11 - Part one

---
+++ src/Day11.kt
@@ -0,0 +1,30 @@
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        val galaxies = input.flatMapIndexed { y, line ->
+            line.mapIndexed { x, c -> c.takeIf { it == '#' }?.let { x to y } }.filterNotNull()
+        }
+        val expandedX = input.indices.filter { !input[it].contains('#') }
+        val expandedY = input[0].indices.filter { !input.map { line: String -> line[it] }.contains('#') }
+
+        return galaxies.flatMap { index ->
+            val others = galaxies.filter { it != index }
+
+            others.zip(List(others.size) { index }).map { (p1, p2) ->
+                val xCount = expandedY.count { it in min(p1.first, p2.first)..max(p1.first, p2.first) }
+                val yCount = expandedX.count { it in min(p1.second, p2.second)..max(p1.second, p2.second) }
+
+                abs(p2.first - p1.first) + xCount + abs(p2.second - p1.second) + yCount
+            }
+        }.let { it.sum() / 2 }
+    }
+
+    val testInput = readInput("Day11_test1")
+    check(part1(testInput) == 374)
+
+    val input = readInput("Day11")
+    part1(input).println()
+}
